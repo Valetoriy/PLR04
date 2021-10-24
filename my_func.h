bd4a3b837dfb15f769d21d628bd38d7ed17d300d
shadolproff <avkryloff2003@mail.ru>
2021-10-24 17:48:59 +0300

    init

---
+++ my_func.h
@@ -0,0 +1,53 @@
+struct vec2 {
+    double x1, x2;
+};
+
+// Разложение на множители
+inline struct vec2 f1(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b};
+}
+
+// Выделение полного квадрата
+inline struct vec2 f2(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение по формуле
+inline struct vec2 f3(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение с использованием теоремы Виета
+inline struct vec2 f4(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение способом "переброски"
+inline struct vec2 f5(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Свойства коэффициентов квадратного уравнения
+inline struct vec2 f6(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Графическое решение
+inline struct vec2 f7(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение с помощью циркуля и линейки
+inline struct vec2 f8(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение с помощью номограммы
+inline struct vec2 f9(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
+
+// Решение геометрическим способом
+inline struct vec2 f10(double a, double b, double c) {
+    return (struct vec2){ a + b + c, b };
+}
