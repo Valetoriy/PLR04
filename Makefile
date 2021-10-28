3ed4fc79e7ce9b186933d86679c81e9ad43b578a
shadolproff <avkryloff2003@mail.ru>
2021-10-28 23:03:05 +0300

    4 + makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc main.c -o lr04 -lm
--- my_func.h
+++ my_func.h
@@ -55,8 +55,8 @@ void f2() {
     } else if (temp2 < 0) {
         printf("x^2 + (%.1lf)x + (%.1lf) <=> (x + (%.1lf))^2 + (%.1lf)\n", b, c,
                temp, temp2);
-        printf("==> x1 = %.1lf, x2 = %.1lf\n", sqrt(-temp2) - temp, -sqrt(-temp2) - temp);
-
+        printf("==> x1 = %.1lf, x2 = %.1lf\n", sqrt(-temp2) - temp,
+               -sqrt(-temp2) - temp);
     }
 }
 
@@ -76,15 +76,13 @@ void f3() {
         double x1 = X1(a, b, d);
         double x2 = X2(a, b, d);
 
-        printf("(%.1lf)x^2 + (%.1lf)x + (%.1lf), D = (%.1lf)\n",
-               a, b, c, d);
+        printf("(%.1lf)x^2 + (%.1lf)x + (%.1lf), D = (%.1lf)\n", a, b, c, d);
         printf("==> x1 = %.1lf, x2 = %.1lf\n", x1, x2);
 
     } else if (d == 0) {
         double x0 = X0(a, b);
 
-        printf("(%.1lf)x^2 + (%.1lf)x + (%.1lf), D = 0\n",
-               a, b, c);
+        printf("(%.1lf)x^2 + (%.1lf)x + (%.1lf), D = 0\n", a, b, c);
         printf("==> x = %.1lf\n", x0);
 
     } else if (d < 0)
@@ -92,7 +90,55 @@ void f3() {
 }
 
 // Решение с использованием теоремы Виета
-void f4() {}
+void f4() {
+    double p, q;
+
+    puts("Способ - решение с использованием теоремы Виета");
+    puts("Уравнение вида x^2 + px + q = 0, (p, q) - int");
+    puts("Введите коэффициенты уравнения:");
+    scanf("%lf%lf", &p, &q);
+
+    double d = D(1, p, q);
+
+    if (d > 0) {
+        double x1 = X1(1, p, d);
+        double x2 = X2(1, p, d);
+
+        printf("x^2 + (%.1lf)x + (%.1lf)\n", p, q);
+        printf("x1 * x2 = q = (%.1lf), x1 + x2 = -p = -(%.1lf)\n", q, p);
+        if (q > 0) {
+            puts("Т.к. q > 0 => x1 и x2 одинаковы по знаку");
+            if (p < 0)
+                puts("Т.к. p < 0 => x1 и x2 - положительны");
+            else if (p > 0)
+                puts("Т.к. p > 0 => x1 и x2 - отрицательны");
+        } else if (q < 0) {
+            puts("Т.к. q < 0 => x1 и x2 различны по знаку");
+            if (p < 0)
+                puts("Т.к. p < 0 => больший по модулю корень будет "
+                     "положительный");
+            else if (p > 0)
+                puts("Т.к. p > 0 => больший по модулю корень будет "
+                     "отрицательный");
+        }
+        printf("==> x1 = %.1lf, x2 = %.1lf\n", x1, x2);
+
+    } else if (d == 0) {
+        double x0 = X0(1, p);
+
+        printf("x^2 + (%.1lf)x + (%.1lf), D = 0\n", p, q);
+        if (q > 0) {
+            puts("Т.к. q > 0 => x1 и x2 одинаковы по знаку");
+            if (p < 0)
+                puts("Т.к. p < 0 => x1 и x2 - положительны");
+            else if (p > 0)
+                puts("Т.к. p > 0 => x1 и x2 - отрицательны");
+        }
+        printf("==> x1 = x2 = %.1lf\n", x0);
+
+    } else if (d < 0)
+        puts("У данного уравнения нет корней!");
+}
 
 // Решение способом "переброски"
 void f5() {}
